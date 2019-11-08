28b3f6253e6402c9c376721cdea834c73772083e
xiangyang <xiangyang@growingio.com>
2019-11-08 17:41:02 +0800

    [feature] Android 添加埋点RN SDK

--- example/App.js
+++ example/App.js
@@ -1,114 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  Switch,
+  Button,
+  StyleSheet,
+  NativeModules,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class RNApp extends Component {
+  state = {
+    isEnable: false,
+  };
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            paddingTop: 10,
+          }}>
+          <Text style={{fontSize: 25}}>触达开关</Text>
+          <Switch
+            style={{marginLeft: 30, transform: [{scaleX: 1.3}, {scaleY: 1.3}]}}
+            value={this.state.isEnable}
+            onValueChange={value => {
+              this.setState({
+                isEnable: value,
+              });
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="触发一个埋点事件"
+            onPress={event => {
+              console.log('xxxx ' + event);
+              NativeModules.GrowingIO.track('touch1', {});
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="打开一个页面"
+            onPress={event => {
+              console.log('xxxx ' + event);
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="注册弹窗监听"
+            onPress={event => {
+              console.log('xxxx ' + event);
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="注销弹窗监听"
+            onPress={event => {
+              console.log('xxxx ' + event);
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    paddingTop: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+    transform: [{scaleX: 1.3}, {scaleY: 1.3}],
   },
 });
-
-export default App;
